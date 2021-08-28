664431a7b88c4b519b8efc9c1c38c9f0f81ab623
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-08-28 15:07:35 +0300

    Update copying

--- copying.py
+++ copying.py
@@ -1,4 +1,5 @@
 from shutil import copyfile
+from photoshop import Session
 from os import mkdir
 
 
@@ -7,15 +8,26 @@ def num2str(num: int, length: int = 2):
     return '0' * (length - len(str(num))) + str(num)
 
 
-ch = 30
-pgnum = 45
+sizes = [(26, 44), (27, 51), (28, 45), (29, 41), (30, 44)]
 
-try:
-    mkdir(num2str(ch, 3))
-except FileExistsError:
-    pass
+for ch, pgnum in sizes:
+    try:
+        mkdir(num2str(ch, 3))
+    except FileExistsError:
+        pass
 
-for pg in range(1, pgnum, 5):
-    expg = pg + 4 if pg + 4 <= pgnum else pgnum
-    copyfile(f'template.psd',
-             f'{num2str(ch, 3)}/Aot-{num2str(ch, 3)}-st({num2str(pg, 2)}-{num2str(expg, 2)}).psd', )
+    for pg in range(1, pgnum, 5):
+        expg = pg + 4 if pg + 4 <= pgnum else pgnum
+        new_file_name = f'{num2str(ch, 3)}/Aot-{num2str(ch, 3)}-st({num2str(pg, 2)}-{num2str(expg, 2)}).psd'
+        copyfile(f'template.psd', new_file_name)
+        print(new_file_name, end=' - ')
+
+        with Session(new_file_name, action="open") as ps:
+            for i, num in enumerate(range(pg, expg+1)):
+                ps.active_document.layers[i].name = num2str(num, 2)
+                print('.', end='')
+            ps.active_document.saveAs(
+                new_file_name, ps.PhotoshopSaveOptions(), True)
+            ps.active_document.close()
+
+        print()
